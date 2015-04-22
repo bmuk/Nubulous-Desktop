9a0bd11acaebe5372123f2c38f732d1247cb4181
Britt Mathis <britt.mathis@gmail.com>
2015-04-22 15:10:08 -0400

    folder dialog!

--- Nubulous-Desktop/Nubulous-Desktop/Form1.cs
+++ Nubulous-Desktop/Nubulous-Desktop/Form1.cs
@@ -19,7 +19,20 @@ namespace Nubulous_Desktop
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            if (textBox1.Text == "")
+            {
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }
+            else
+            {
+                if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+                {
+                    textBox2.Text = folderBrowserDialog2.SelectedPath;
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
