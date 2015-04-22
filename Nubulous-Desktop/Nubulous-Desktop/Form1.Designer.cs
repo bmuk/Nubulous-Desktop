0b43e25304b23c0822039483588a8383b63fc8c4
patjlouis <patjlouis@gmail.com>
2015-04-22 15:24:01 -0400

    DOCK

--- Nubulous-Desktop/Nubulous-Desktop/Form1.Designer.cs
+++ Nubulous-Desktop/Nubulous-Desktop/Form1.Designer.cs
@@ -40,10 +40,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(472, 394);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(354, 320);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Next";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,10 +51,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(448, 325);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(336, 264);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(41, 39);
+            this.button2.Size = new System.Drawing.Size(31, 32);
             this.button2.TabIndex = 1;
             this.button2.Text = "+";
             this.button2.UseVisualStyleBackColor = true;
@@ -66,43 +64,40 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(93, 46);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(70, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(349, 35);
+            this.label1.Size = new System.Drawing.Size(269, 27);
             this.label1.TabIndex = 3;
             this.label1.Text = "Choose Drives to Intergrate";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(99, 128);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(74, 104);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(343, 22);
+            this.textBox1.Size = new System.Drawing.Size(258, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(99, 223);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(74, 181);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(343, 22);
+            this.textBox2.Size = new System.Drawing.Size(258, 20);
             this.textBox2.TabIndex = 5;
             // 
             // Nubulous
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(588, 437);
+            this.ClientSize = new System.Drawing.Size(441, 355);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
             this.Name = "Nubulous";
             this.Text = "Nubulous";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Nubulous-Desktop/Nubulous-Desktop/Form2.Designer.cs
+++ Nubulous-Desktop/Nubulous-Desktop/Form2.Designer.cs
@@ -75,6 +75,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form2";
             this.Text = " Nubulous";
             this.ResumeLayout(false);
