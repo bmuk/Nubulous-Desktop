ad6d822496611a288b72cd0df3d4c45b1832c290
patjlouis <patjlouis@gmail.com>
2015-04-22 14:03:59 -0400

    Sync

--- Nubulous-Desktop/Nubulous-Desktop/Form1.cs
+++ Nubulous-Desktop/Nubulous-Desktop/Form1.cs
@@ -16,5 +16,15 @@ namespace Nubulous_Desktop
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
